7a37c6b3729f8f243af6191cfdf70e05b749cd57
Halyna Zhuk <104450263+halyna-zhuk@users.noreply.github.com>
2022-07-01 19:01:12 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,99 @@
+""" lst = [1, 2, 3, 4, 5]
+lst_new = []
+for i in lst:
+    if i % 2 == 0:
+        lst_new.append(i)
+print(lst_new[i])
+
+"""
+""" n_1 = 10 # int
+n = 3.64564 #float
+
+print(n_1)
+print(n) 
+
+n: int = 20
+print(n)
+
+text = "Hi 'everybody'!!!" #str
+print(text)
+"""
+lst = [1, 2, 3, ["rtre", "wr"], "rwetfrwefwed", 777] #list
+# print(lst)
+# print(id(lst))
+# n = 45
+# lst.append(n)
+# print(lst)
+# print(id(lst))
+"""n = 45,
+t = (34, 34, 65, 56)
+print(t)
+print(id(t))
+t += n
+print(t)
+
+print(type(n))
+print(id(t)) 
+s = {1, 1, 1, 2, 2}
+print(s) 
+n = "rwtfwfw"
+s = {}
+print(list(n))
+print(set(n))
+
+dct = {'Jon': 653343543,
+       'person': {
+           'name': 'Lida',
+           'age': 45,
+           'address': 'Lviv, Giga 34'
+       },'person_2': {
+           'name': 'Lida'}}
+print(dct) """
+
+# print(5 + '1')
+#print(5 + True)
+#print(5 / False)
+"""
+a = 7
+b = 7
+if a > b:
+
+    print(a > b)
+elif a < b:
+    print('a < b')
+else:
+    print('else')
+
+
+lst = [1, 2, 3, 4]
+lst_new = []
+print(lst)
+for i in lst:
+    print(i)
+    if type(i) == int:
+        if i % 2 == 0:
+            lst_new.append(i)
+print(lst_new) 
+
+def f():
+    print("343434")
+f()
+f() """
+def f(name):
+    print(f'Hi {name}')
+f('Jon')
+f('Gala')
+f('Bob')
+def func(a, y):
+    return (a - y)
+n = func(56,50)
+print(n)
+
+
+
+
+
+
+
+
+
